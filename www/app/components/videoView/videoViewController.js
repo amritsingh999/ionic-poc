dfb93bd22cf580eaa4e2c6175e38fef65624db49
Amritpal Singh <Amritpal Singh>
2021-04-10 14:03:02 -0700

    style video view

--- www/app/components/videoView/videoViewController.js
+++ www/app/components/videoView/videoViewController.js
@@ -1,6 +1,10 @@
 app.controller('videoViewController', ['$scope', 
     function($scope) {
         console.log('loaded controller for video view.');
-        
+
+
+        $scope.endCall = function() {
+            console.log('end Call');
+        }
     }
 ]);
